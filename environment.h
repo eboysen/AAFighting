b153b6c17423c9ed75d0912cf1341ea0505f70c6
Brandon <bmc939@gmail.com>
2021-11-14 03:03:25 -0600

    No more memory leak

--- environment.h
+++ environment.h
@@ -7,10 +7,10 @@ class environment
 {
 public:
 	environment();
+	environment(SDL_Renderer* render);
 	~environment();
 	void renderEnvironment(SDL_Renderer * render, int width, int height);
 	void platformCheck(character* Character);
 	void characterCollided(character* Character, double yPos);
-
 };
 
